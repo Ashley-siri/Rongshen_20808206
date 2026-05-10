3e0e64dcd40d0e791e9538d20dc816fdeb7a0cf4
Ashley-siri <401412130@qq.com>
2026-05-10 11:28:08 +0800

    Add files via upload

---
+++ src/data/tag_categories.js
@@ -0,0 +1,7 @@
+const userDefinedTagCategories = [
+    {
+        name: "Type",
+        description: "type of the paper",
+        tags: ["article", "techreport", "misc", "inproceedings", "book"]
+    }
+];
